168a9490cc86db78b57ddd8611d0ba2b6b1fbaa9
Gustavo Vicenzi <gusvicenzi12@gmail.com>
2022-02-28 18:17:37 -0300

    Lesson 4

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,26 @@
+const Database = require('../db/config');
+
 module.exports = {
-  create(req, res) {
-    let room = 123456;
+  async create(req, res) {
+    const db = await Database();
+    const pass = req.body.password;
+    let room;
+
+    for (var i = 0; i < 6; i++) {
+      i == 0
+        ? (room = Math.floor(Math.random() * 10).toString())
+        : (room += Math.floor(Math.random() * 10).toString());
+    }
+
+    await db.run(`INSERT INTO rooms (
+      id,
+      pass
+    ) VALUES (
+      ${parseInt(room)},
+      ${pass}
+    )`);
+
+    await db.close();
 
     res.redirect(`/room/${room}`);
   }
